7a103a8f9e5c9218e94de67fd8a5e814d6f7aa19
Smolnikov Ivan <Smolnikov.Ivan@lenta.ru>
2022-08-07 01:40:08 +0300

    task4

---
+++ Homework task4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Enter the number N to which you want to select even starting with the number one: ");
+int numberN = int.Parse(Console.ReadLine());
+for (int i = 1; i <= numberN; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
+
+//Интересное выравнивание делает Alt+Shift+F
