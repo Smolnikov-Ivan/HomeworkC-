ce19f7c29e0ac32beea7c129cde7d0c3bad5b959
Smolnikov Ivan <Smolnikov.Ivan@lenta.ru>
2022-08-07 01:39:47 +0300

    task3

---
+++ Homework task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+
+Console.WriteLine("Determining the parity of the entered number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if ((number1 % 2) == 0)
+{
+    Console.WriteLine("The entered number is even.");
+}
+else
+{
+    Console.WriteLine("The entered number is not even.");
+}
