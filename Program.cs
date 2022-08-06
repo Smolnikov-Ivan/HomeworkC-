3e3f5f5dcfe295f8c2224a9430ec6db459af4f79
Smolnikov Ivan <Smolnikov.Ivan@lenta.ru>
2022-08-06 15:27:36 +0300

    new out home

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+    if (number1 > number2)
+    {
+    Console.WriteLine("Первое число является бОльшим.");
+    Console.WriteLine(number1);
+    }
+    else 
+    {
+    Console.WriteLine("Второе число является бОльшим.");    
+    Console.WriteLine(number2);
+    }
+
+//алгоритм работает полностью, хоть и на Console подчёркивание красное, ошибок нет.
+
+//string name = Console.RedLine();
+//Console.ReadLine() ждём значение из терминала
+//Convert.ToInt32() превращаем значение в число из терминала
+//Создаём переменную и записываем значение туда 
+// = присвоить
+// == сравнить
+// if() {}
+
